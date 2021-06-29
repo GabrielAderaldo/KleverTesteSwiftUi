c354190a34c8a3c6f6f100cdf8af62ed30e2fb19
Gabriel Aderaldo <gabrieladeraldo@MacMini-2018.local>
2021-06-29 14:33:10 -0300

    Hello world

---
+++ KleverTeste/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  KleverTeste
+//
+//  Created by Gabriel Aderaldo on 28/06/21.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack{
+            Text("Começando...")
+        }.padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+
+//Criando os componentes...
+
+

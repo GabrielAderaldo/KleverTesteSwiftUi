ec871de70cf4a4b12717482565ba6dcc1f02ed62
Gabriel Aderaldo <gabrieladeraldo@MacMini-2018.local>
2021-06-29 16:29:01 -0300

    Tela de login, sem a logica dos request.

--- KleverTeste/ContentView.swift
+++ KleverTeste/ContentView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    
     var body: some View {
         
-        VStack{
-            Text("Começando...")
-        }.padding()
-        
+        ZStack{
+            //Criando o gradiente...
+            RadialGradient(gradient: Gradient(colors: [Color.red, Color.purple]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 100, endRadius: 470)
+            
+            VStack{
+                
+                LogoViewEeApresentacao()
+                SubTemaText()
+                Inpults()
+                Bnt()
+                
+            }
+            
+        }
     }
 }
 
@@ -25,11 +37,92 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
+///MARK:Components
+
+///MARK:Logo
+struct LogoViewEeApresentacao: View {
+    
+    var body: some View{
+        
+        Image("logo")
+            .resizable()
+            .aspectRatio(contentMode: ContentMode.fit)
+            .frame(width: 74.0, height: 74.0)
+            .padding(Edge.Set.bottom, 20)
+        
+        
+        Text("Login").bold().font(.title).foregroundColor(.white)
+        
+    }
+}
+
+///MARK:  SubTema...
+
+struct SubTemaText: View {
+    
+ 
+    
+    var body: some View{
+        
+        Text("Feito por Gabriel Aderaldo")
+            .font(.subheadline)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 70, trailing: 0))
+            .foregroundColor(.white)
+        
+    }
+}
+
+///MARK: Inpults de login e senha
+
+struct Inpults : View {
+    
+    @State private var name: String = ""
+    @State private var senha: String = ""
+    
+    var body: some View{
+        
+        TextField("Login", text: $name)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(4.0)
+            .padding(EdgeInsets(top: 0, leading: 10, bottom: 15, trailing: 10))
+        
+        TextField("Senha", text: $senha)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(4.0)
+            .padding(EdgeInsets(top: 0, leading: 10, bottom: 15, trailing: 10))
+        
+    }
+}
 
 
+///MARK: Butões
 
-
-
-//Criando os componentes...
-
+struct Bnt : View {
+    
+    var body: some View{
+        
+        Button(action: {
+            
+        }, label: {
+            Text("Login")
+        })
+        .padding()
+        .background(Color.white)
+        .cornerRadius(8)
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+        .foregroundColor(.black)
+        
+        
+        
+        Button(action: {
+            print("Butão acessado...")
+        }, label: {
+            Text("Não tem conta? clique aqui!")
+        }).padding(EdgeInsets(top: 0, leading: 10, bottom: 10, trailing: 0)).foregroundColor(Color.white)
+        
+    }
+    
+}
 
